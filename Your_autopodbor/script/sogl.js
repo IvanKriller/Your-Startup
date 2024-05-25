6f7c2e87a8ddbcf45108a1d1c1ac496c9d534230
IvanKriller <166858208+IvanKriller@users.noreply.github.com>
2024-05-25 22:12:36 +0300

    Add files via upload

---
+++ Your_autopodbor/script/sogl.js
@@ -0,0 +1,28 @@
+ function changePhoto() {
+            var currentPhoto = document.querySelector('.photo');
+            if (currentPhoto.src.includes('img/Gal0.png')) {
+                currentPhoto.src = 'img/Gal1.png';
+            } else {
+                currentPhoto.src = 'img/Gal0.png';
+            }
+}
+
+function myFunction_1() {
+  document.getElementById("myDropdown_1").classList.toggle("show");
+}
+
+function myFunction_2() {
+  document.getElementById("myDropdown_2").classList.toggle("show");
+}
+
+function myFunction_3() {
+  document.getElementById("myDropdown_3").classList.toggle("show");
+}
+
+function myFunction_4() {
+  document.getElementById("myDropdown_4").classList.toggle("show");
+}
+
+function myFunction_5() {
+  document.getElementById("myDropdown_5").classList.toggle("show");
+}
